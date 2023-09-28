99735fb45e4f198ebf76e2e475299f4501a8e090
Mitch Phillipson <mitch.phillipson@gmail.com>
2023-09-28 12:03:44 -0500

    Add new packages

--- src/WiNDC.jl
+++ src/WiNDC.jl
@@ -3,15 +3,23 @@ module WiNDC
 
 using JuMP
 using PATHSolver
+using Ipopt
 #using Complementarity
 using GamsStructure
 using CSV 
 using XLSX
+using DataFrames
+using HTTP
+using JSON
 
-export national_model_mcp,load_bea_data,state_dissagregation_model_mcp_year
+export national_model_mcp,load_bea_data_api,load_bea_data_local,calibrate_national!
+
+#,state_dissagregation_model_mcp_year
 
 
 include("core/nationalmodel.jl")
+include("data/calibrate.jl")
 include("data/PartitionBEA.jl")
+
 
 end # module WiNDC
