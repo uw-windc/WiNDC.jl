eb5ca1592961b7336d3bb7e8c836bef8b2f594d7
Mitch Phillipson <mitch.phillipson@gmail.com>
2023-09-28 13:03:15 -0500

    Adding helper functions for modelling

--- src/WiNDC.jl
+++ src/WiNDC.jl
@@ -12,14 +12,16 @@ using DataFrames
 using HTTP
 using JSON
 
-export national_model_mcp,load_bea_data_api,load_bea_data_local,calibrate_national!
+export national_model_mcp,load_bea_data_api,load_bea_data_local, verify_calibration, generate_report
 
 #,state_dissagregation_model_mcp_year
 
 
-include("core/nationalmodel.jl")
-include("data/calibrate.jl")
+include("helper_functions.jl")
+
 include("data/PartitionBEA.jl")
+include("core/nationalmodel.jl")
+
 
 
 end # module WiNDC
---
+++ src/helper_functions.jl
@@ -0,0 +1,33 @@
+extract_variable_ref(v::NonlinearExpr) = v.args[1]
+extract_variable_ref(v::AffExpr) = collect(keys(v.terms))[1]
+extract_variable_ref(v::QuadExpr) = extract_variable_ref(v.aff)
+
+function generate_report(m::JuMP.Model;decimals::Int = 4)
+
+    mapping = Dict()
+    for ci in all_constraints(m; include_variable_in_set_constraints = false)
+        c = constraint_object(ci)
+        mapping[extract_variable_ref(c.func[2])] = c.func[1]
+    end
+
+    out = "var_name\t value\t\t margin\n"
+    for elm in all_variables(m)
+
+        val = round(value(elm),digits = decimals)
+        margin = "."
+        try
+            margin = round(value(mapping[elm]),digits = decimals)
+        catch
+            margin = "."
+        end
+        
+
+        out = out*"$elm\t\t $val\t\t $margin\n"
+    end
+
+    return(out)
+end
+
+function verify_calibration(m::JuMP.Model)
+    sum([abs(a) for (a,b) in value.(all_constraints(m; include_variable_in_set_constraints = false))])
+end
