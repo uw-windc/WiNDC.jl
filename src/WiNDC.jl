f3211105b16caf41324bb37b7ae4705eac4130ac
mitchphillipson <mitch.phillipson@gmail.com>
2023-11-08 13:03:14 -0600

    Add new files

--- src/WiNDC.jl
+++ src/WiNDC.jl
@@ -12,24 +12,35 @@ using DataFrames
 using HTTP
 using JSON
 
-export national_model_mcp,load_bea_data_api,load_bea_data_local, verify_calibration, generate_report
+export generate_report
+
+export national_model_mcp,  state_dissagregation_model_mcp_year
+
+
+#load_bea_data_api, load_bea_data_local, verify_calibration,
 
 #,state_dissagregation_model_mcp_year
 
 
 include("helper_functions.jl")
 include("data/notations.jl")
+
 include("data/core/bea_api/bea_api.jl")
 
-include("data/core/PartitionBEA.jl")
+#Models
 include("core/nationalmodel.jl")
+include("core/state_dissagregation_model.jl")
 
+#Data
+include("data/core/bea_io/PartitionBEA.jl")
 include("data/core/bea_gsp/bea_gsp.jl")
 include("data/core/bea_pce/bea_pce.jl")
 include("data/core/census_sgf/census_sgf.jl")
 include("data/core/faf/faf.jl")
 include("data/core/usa_trade/usa_trade.jl")
+include("data/core/state_dissagregation.jl")
 
+include("data/core/core_data.jl")
 
 
 end # module WiNDC
