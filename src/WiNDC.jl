4cbb5fc42f9f7ed5a252b3b43091c7d1d828fc0e
mitchphillipson <mitch.phillipson@gmail.com>
2024-09-05 11:46:45 -0500

    Remove MPSGE

--- src/WiNDC.jl
+++ src/WiNDC.jl
@@ -1,9 +1,22 @@
 module WiNDC
 
 
-using DataFrames, CSV, XLSX, FileIO, JuMP, Ipopt, MPSGE
+using DataFrames, CSV, XLSX, FileIO, JuMP, Ipopt#, MPSGE
 
+include("structs.jl")
 
+export all_data, domain, WiNDCtable, extract_set
 
+include("national/national_data.jl")
+
+export load_raw_national_summary_data
+
+include("national/calibrate.jl")
+
+export calibrate
+
+include("national/balance.jl")
+
+export zero_profit, market_clearance, margin_balance
 
 end # module WiNDC
