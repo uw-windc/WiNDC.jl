232bfb5ba3417c5798bdc85d9a12c9d5f14774a0
mitchphillipson <mitch.phillipson@gmail.com>
2024-10-15 11:15:05 -0500

    Add output names and init in sums

--- src/national/balance.jl
+++ src/national/balance.jl
@@ -4,12 +4,12 @@ function zero_profit(data::WiNDCtable; column = :value, output = :zero_profit)
 
     ag_columns = filter(y -> y!=:commodities, domain(data))
     return vcat(
-            get_subtable(data, "intermediate_demand", column = column),
-            get_subtable(data, "value_added", column = column), 
-            get_subtable(data, "intermediate_supply", column = column, negative = true) 
+            get_subtable(data, "intermediate_demand", column = column, output = output),
+            get_subtable(data, "value_added", column = column, output = output), 
+            get_subtable(data, "intermediate_supply", column = column, output = output, negative = true) 
         ) |> 
         x -> groupby(x, ag_columns) |>
-        x -> combine(x, column => sum => output)    
+        x -> combine(x, output => (y -> sum(y;init=0)) => output)    
 end
 
 
@@ -17,26 +17,26 @@ function market_clearance(data::WiNDCtable; column = :value, output = :market_cl
     ag_columns = filter(y -> y!=:sectors, domain(data))
 
         return vcat(
-            get_subtable(data, "intermediate_demand", column = column) ,
-            get_subtable(data, "final_demand", column = column),
-            get_subtable(data, "household_supply", column = column, negative = true),
+            get_subtable(data, "intermediate_demand", column = column, output = output) ,
+            get_subtable(data, "final_demand", column = column, output = output),
+            get_subtable(data, "household_supply", column = column, output = output, negative = true),
 
-            get_subtable(data, "intermediate_supply", column = column, negative = true),
-            get_subtable(data, "imports", column = column, negative = true),
-            get_subtable(data, "margin_demand", column = column, negative = true),
-            get_subtable(data, "margin_supply", column = column), # Made negative earlier
-            get_subtable(data, "duty", column = column, negative = true),
-            get_subtable(data, "tax", column = column, negative = true),
-            get_subtable(data, "subsidies", column = column) # Made negative earlier
+            get_subtable(data, "intermediate_supply", column = column, output = output, negative = true),
+            get_subtable(data, "imports", column = column, output = output, negative = true),
+            get_subtable(data, "margin_demand", column = column, output = output, negative = true),
+            get_subtable(data, "margin_supply", column = column, output = output), # Made negative earlier
+            get_subtable(data, "duty", column = column, output = output, negative = true),
+            get_subtable(data, "tax", column = column, output = output, negative = true),
+            get_subtable(data, "subsidies", column = column, output = output) # Made negative earlier
         ) |>
         x -> groupby(x, ag_columns) |>
-        x -> combine(x, column => sum => output)
+        x -> combine(x, output => (y -> sum(y;init=0)) => output)
 end
 
 margin_balance(data::WiNDCtable; column = :value, output = :margin_balance) =
     vcat(
-        WiNDC.get_subtable(data, "margin_supply", column = column, negative = true),
-        WiNDC.get_subtable(data, "margin_demand", column = column)
+        WiNDC.get_subtable(data, "margin_supply", column = column, output = output, negative = true),
+        WiNDC.get_subtable(data, "margin_demand", column = column, output = output)
     ) |>
     x -> groupby(x, filter(y -> y!=:commodities, domain(data))) |>
-    x -> combine(x, column => sum => output)
+    x -> combine(x, output => (y -> sum(y;init=0)) => output)
