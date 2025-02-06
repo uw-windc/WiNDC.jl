d3e209b02a930ac97bd3cb5e305c4c8ce3359e19
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-02-06 08:39:32 -0600

    Documentation - Simplify

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,8 @@ using Documenter, WiNDC, PATHSolver
 
 const _PAGES = [
     "Introduction" => ["index.md"],
-    "Data" => ["data/core.md"],
-    "National Module" => ["national/overview.md"]
-    #"Core Module" => ["core/overview.md","core/national_model.md","core/national_set_list.md","core/state_model.md","core/set_listing.md"]
+    #"Data" => ["data/core.md"],
+    #"National Module" => ["national/overview.md"],
     "API" => ["api.md"]
     
 ]
