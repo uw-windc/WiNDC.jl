186c103887f3d97fb2b924ec3fc684da1e3bedd7
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-02-11 09:55:18 -0600

    Remove unused exports

--- src/WiNDC.jl
+++ src/WiNDC.jl
@@ -9,7 +9,7 @@ using ZipFile, Downloads
 
 include("structs.jl")
 
-export all_data, domain, WiNDCtable, get_set, get_table, get_subtable
+export domain, WiNDCtable, get_set, get_table, get_subtable
 
 include("io.jl")
 
@@ -29,7 +29,6 @@ export NationalTable
 
 include("national/detailed_data.jl")
 
-export national_tables
 
 include("national/load_data.jl")
 
@@ -46,10 +45,6 @@ export zero_profit, market_clearance, margin_balance
 include("national/model.jl")
 
 export national_mpsge
-
-include("national/download.jl")
-
-export fetch_supply_use
 
 ## State Data
 
